93d40b99a7afd63b7b7917d63966e98d8781c0c9
Hawkay002 <shovith2002@gmail.com>
2026-08-06 16:39:31 +0530

    fix: all modals + dropdowns fully solid black (rgb 0 0 0 / 1)
    - DialogContent (all modals): solid black override
    - SelectContent (ticket type, gender, export format): solid black override
    - Select scroll buttons: bg-black
    - guest list + logs custom dropdowns: already solid black from prior commit
    - help tray: already solid black

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -56,6 +56,7 @@ function DialogContent({
           "glass-panel fixed top-1/2 left-1/2 z-50 grid w-full max-w-[calc(100%-2rem)] -translate-x-1/2 -translate-y-1/2 gap-4 p-4 text-sm text-popover-foreground duration-100 outline-none sm:max-w-sm data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 data-closed:animate-out data-closed:fade-out-0 data-closed:zoom-out-95",
           className
         )}
+        style={{ background: "rgb(0 0 0 / 1)" }}
         {...props}
       >
         {children}
